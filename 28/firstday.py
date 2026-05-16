47b532cf3bf5167816b2e04b68b5ab26d60c226e
Your Name <cannel5251@gmail.com>
2026-05-16 18:31:54 +1000

    test

---
+++ 28/firstday.py
@@ -0,0 +1,10 @@
+#print(object, separator= default space, end)
+print('hello world')
+print('hi there')
+
+a=100
+b=200
+c=300
+
+print(a, b, c) #100 200 300
+print(a, b, c, sep='@@') #100@@200@@300
